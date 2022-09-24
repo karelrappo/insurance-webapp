8bfe8d563da358dec35bc06f6f6e26d9144e9077
krappo <karelrappo@gmail.com>
2022-09-24 13:39:43 +0200

    Converted from pycaret to sklearn

--- web-app.py
+++ web-app.py
@@ -1,12 +1,14 @@
 from flask import Flask,request, url_for, redirect, render_template, jsonify
-from pycaret.regression import *
+from lightgbm import LGBMRegressor
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder,PowerTransformer
+from sklearn.compose import ColumnTransformer
 import pandas as pd
-import pickle
+import joblib
 import numpy as np
 
 app = Flask(__name__)
 
-model = load_model('deployment_gbr')
+regressor = joblib.load('model.pkl')
 cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 
 @app.route('/')
@@ -18,16 +20,16 @@ def predict():
     int_features = [x for x in request.form.values()]
     final = np.array(int_features)
     data_unseen = pd.DataFrame([final], columns = cols)
-    prediction = predict_model(model, data=data_unseen, round = 0)
-    prediction = int(prediction.Label[0])
+    prediction = regressor.predict(data_unseen)
+    prediction = int(prediction[0])
     return render_template('home.html', pred='Expected Bill will be {}'.format(prediction))
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
-    prediction = predict_model(model, data=data_unseen)
-    output = prediction.Label[0]
+    prediction = regressor.predict(data_unseen)
+    output = prediction[0]
     return jsonify(output)
 
 if __name__ == '__main__':
